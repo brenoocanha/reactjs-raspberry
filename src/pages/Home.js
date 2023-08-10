18cbdc759d0241555c82f4132db5713c86932fb4
Breno Ocanha <breno@dmxdesign.com.br>
2023-08-10 18:06:59 -0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Titulo = styled.h1`
+  font-family: 'Lora', sans-serif;
+`
+
+export default function Home() {
+  return (
+    <>
+      <Titulo>Test</Titulo>
+    </>
+  )
+}
